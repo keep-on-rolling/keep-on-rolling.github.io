d8a7bd158067c160d37f9e3d2287429488de172c
Marcus Fihlon <marcus@fihlon.swiss>
2022-06-17 18:15:51 +0200

    ⚡️ Add event listener only on german pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,10 @@
 /* Temporary fix for https://github.com/lxndrblz/anatole/issues/369  */
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (window.location.pathname.startsWith("/de/")) {
+if (window.location.pathname.startsWith("/de/")) {
+    document.addEventListener('DOMContentLoaded', (event) => {
         Array.from(document.getElementsByTagName("h3")).forEach(function (element) {
             if (element.innerHTML === "Posts in this Series") {
                 element.innerHTML = "Beiträge in dieser Serie";
             }
         });
-    }
-});
+    });
+}
