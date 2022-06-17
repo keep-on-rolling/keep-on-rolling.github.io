2ed27883ae103e5ab479dab6ed609c5085dccb38
Marcus Fihlon <marcus@fihlon.swiss>
2022-06-17 18:12:46 +0200

    🐛 Fix custom JS breaks medium zoom image

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,10 @@
 /* Temporary fix for https://github.com/lxndrblz/anatole/issues/369  */
 document.addEventListener('DOMContentLoaded', (event) => {
     if (window.location.pathname.startsWith("/de/")) {
-        document.body.innerHTML = document.body.innerHTML.replace(/Posts in this Series/g, 'Beiträge in dieser Serie');
+        Array.from(document.getElementsByTagName("h3")).forEach(function (element) {
+            if (element.innerHTML === "Posts in this Series") {
+                element.innerHTML = "Beiträge in dieser Serie";
+            }
+        });
     }
 });
